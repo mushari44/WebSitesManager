2c4613f070c5d59d53de3bdd63a0f0a3cbcf93c0
mushari44 <meeshoo-010@hotmail.com>
2024-06-23 21:36:50 +0300

    added  HashRouter back

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Form } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Form } from "react-router-dom";
 import TreeView from "./components/tree-view";
 import ScrollIndicator from "./components/scroll-indicator/";
 import LoadMoreData from "./components/load-more-data";
